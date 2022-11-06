6597a65332fd0d3b83170942ffdb0664f60664dc
Remix Workspace <>
2022-11-06 16:16:40 +0300

    Initial Commit

---
+++ contracts/edu/FirstContract.sol
@@ -0,0 +1,22 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.17;
+
+contract FirstContract {
+
+}
+
+contract Inbox {
+    string public message;
+
+    constructor (string memory initialMessage) {
+        message = initialMessage;
+    }
+
+    function setMessage(string memory newMessage) public {
+        message = newMessage;
+    }
+
+    function getMessage() public view returns(string memory) {
+        return message;
+    }
+}
