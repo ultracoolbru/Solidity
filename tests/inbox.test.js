f4454154124280e2dfe96548daa5243d0072788c
Edward Whitehead <edward@ultracoolbru.com>
2022-11-06 22:13:07 +0300

    .

--- tests/inbox.test.js
+++ tests/inbox.test.js
@@ -1,42 +1,31 @@
 const assert = require('assert');
+const expect = require('chai');
 const ganache = require('ganache');
 const Web3 = require('web3');
 const web3 = new Web3(ganache.provider());
-// const { interface, bytecode } = require('../scripts/compile.js');
+const { interface, bytecode } = require('../scripts/compile');
 
 let accounts;
-// let inbox;
-
-// beforeEach(async () => {
-//     // Get a list of all accounts
-//     accounts = await web3.eth.getAccounts();
-
-//     console.log(accounts);
-//     console.log(interface);
-//     console.log(bytecode);
-
-//     // Use one of those accounts to deploy the contract
-//     inbox = await new web3.eth.Contract(JSON.parse(interface))
-//         .deploy({ data: bytecode, arguments: ['Hi there!'] })
-//         .send({ from: accounts[0], gas: '1000000' });
-// });
-
-// describe('Inbox', () => {
-//     it('deploys a contract', () => {
-//         assert.ok(inbox.options.address);
-//     });
-// });
+let inbox;
 
 beforeEach(async () => {
     // Get a list of all accounts
-    accounts = await web3.eth.getAccounts()
-        .then(fetchedAccounts => {
-            console.log(fetchedAccounts);
-        });
+    accounts = await web3.eth.getAccounts();
+
+    console.log(accounts);
+    console.log(interface);
+    console.log(bytecode);
+
+    // Use one of those accounts to deploy the contract
+    inbox = await new web3.eth.Contract(JSON.parse(interface))
+        .deploy({ data: bytecode, arguments: ['Hi there!'] })
+        .send({ from: accounts[0], gas: '1000000' });
 });
 
 describe('Inbox', () => {
     it('deploys a contract', () => {
-        assert.ok(true);
+        //assert.ok(inbox.options.address);
+        expect.ok(inbox.options.address);
+        console.log(inbox); 
     });
 });
